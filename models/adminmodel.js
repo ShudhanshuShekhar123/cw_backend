1e14ba7f144024b9bffeedf63bfd598a4ad8d276
Shudhanshu Shekhar <115460439+ShudhanshuShekhar123@users.noreply.github.com>
2023-09-19 09:36:51 +0530

    update existing file

--- models/adminmodel.js
+++ models/adminmodel.js
@@ -5,6 +5,9 @@ const mongoose = require('mongoose');
 const AdminSchema = new mongoose.Schema({
     name: { type: String, required: true },
     password: { type: String, required: true }
+
+
+
 })
 
 
